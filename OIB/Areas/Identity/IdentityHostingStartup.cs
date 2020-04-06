4acb5e2bc5e21588f55109cb19b4fd0260c811e8
makeitshine <aaytanova80@gmail.com>
2020-04-06 18:22:33 +0600

    gwg

---
+++ OIB/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OIB.Data;
+
+[assembly: HostingStartup(typeof(OIB.Areas.Identity.IdentityHostingStartup))]
+namespace OIB.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OIBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OIBContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<OIBContext>();
+            });
+        }
+    }
+}
